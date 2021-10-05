6efde62bfb7599e946b9ef271de91602c7948f43
StrixV54 <64365738+StrixV54@users.noreply.github.com>
2021-10-05 19:56:05 +0530

    Added dark mode

--- js/script.js
+++ js/script.js
@@ -3,23 +3,9 @@ const addBtn = document.querySelector(".notes-add-button");
 const ccBtn = document.querySelector(".cancel-btn");
 const saveBtn = document.querySelector(".save-btn");
 const todoList = document.querySelector(".notes-list");
-//const deleteAllBtn = document.querySelector(".footer button");
-/*
-addBtn.onclick = ()=>{
-  document.querySelector(".text-preview").style.display="block";
-  document.querySelector(".text-blur").style.display="block";
-}
-ccBtn.onclick = ()=>{
-  document.querySelector(".text-preview").style.display="none";
-  document.querySelector(".text-blur").style.display="none";
-}
-saveBtn.onclick = ()=>{ 
-console.log("Hello");
-let newLiTag = `<div class="notes-list-item">
-<div class="notes-small-body">I learnt nothing today.</div>
-</div>`;
-todoList.innerHTML += newLiTag; }
-*/
+let idx=null;
+//const deleteAllBtn = document.querySelector(".del-button");
+
 addBtn.onclick = ()=>{
   document.querySelector(".text-preview").style.display="block";
   document.querySelector(".text-blur").style.display="block";
@@ -44,12 +30,16 @@ showTasks();
 saveBtn.onclick = ()=>{ 
   let userEnteredValue = inputBox.value;
   if(userEnteredValue.trim() != 0){
-  console.log("savebtn "+userEnteredValue); 
+  //console.log("savebtn "+userEnteredValue); 
   let getLocalStorageData = localStorage.getItem("New Todo"); 
   if(getLocalStorageData == null){ 
     listArray = []; 
   }else{
     listArray = JSON.parse(getLocalStorageData);  
+  }
+  if(idx!=null){
+    listArray.splice(idx, 1); 
+    localStorage.setItem("New Todo", JSON.stringify(listArray));
   }
   listArray.push(userEnteredValue); 
   localStorage.setItem("New Todo", JSON.stringify(listArray));
@@ -77,7 +67,7 @@ function showTasks(){
   }*/
   let newLiTag = "";
   listArray.forEach((element, index) => {
-    newLiTag += `<div class="notes-list-item"><span class="icon"><i class="fas fa-pen-square fa-lg"></i><span class="exit" ondblclick="deleteTask(${index})"><i class="far fa-times-circle fa-lg"></i></span></span>${element}</div></div>`;
+    newLiTag += `<div class="notes-list-item"><span class="icon"><i class="fas fa-pen-square fa-lg" onclick="editTask(${index})"></i><span class="exit" ondblclick="deleteTask(${index})"><i class="far fa-times-circle fa-lg"></i></span></span>${element}</div></div>`;
     //newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
   });
   todoList.innerHTML = newLiTag; 
@@ -92,8 +82,19 @@ function deleteTask(index){
   localStorage.setItem("New Todo", JSON.stringify(listArray));
   showTasks(); 
 }
+function editTask(index){
+  let getLocalStorageData = localStorage.getItem("New Todo");
+  listArray = JSON.parse(getLocalStorageData);
+  let texx=listArray[index];
+  console.log(texx);
+  //listArray.splice(index, 1); 
+  //localStorage.setItem("New Todo", JSON.stringify(listArray));
+  document.querySelector(".text-preview").style.display="block";
+  document.querySelector(".text-blur").style.display="block";
+  inputBox.value=texx;
+  idx=index;
+}
 /*
-// delete all tasks function
 deleteAllBtn.onclick = ()=>{
   let getLocalStorageData = localStorage.getItem("New Todo"); 
   if(getLocalStorageData == null){ 
@@ -105,3 +106,12 @@ deleteAllBtn.onclick = ()=>{
   localStorage.setItem("New Todo", JSON.stringify(listArray)); 
   showTasks(); 
 }*/
+const btn_toggle = document.querySelector(".theme-toggle");
+const theme = document.querySelector("#theme-link");
+      btn_toggle.addEventListener("click", function() {
+        if (theme.getAttribute("href") == "./css/light-style.css") {
+          theme.href = "./css/dark-style.css";
+        } else {
+          theme.href = "./css/light-style.css";
+        }
+      });
