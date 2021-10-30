689e867e883cd551a9c627e7d558524f545440af
StrixV54 <64365738+StrixV54@users.noreply.github.com>
2021-10-30 21:10:36 +0530

    fixed bugs

--- js/app.js
+++ js/app.js
@@ -15,12 +15,15 @@ const inputBox = document.querySelector(".text-body");
 const addBtn = document.querySelector(".notes-add-button");
 const ccBtn = document.querySelector(".cancel-btn");
 const saveBtn = document.querySelector(".save-btn");
+const impBtn = document.querySelector(".imp");
 const todoList = document.querySelector(".notes-list");
 let idx=null; let listArray=[];
+let codeNum=0;
 //const deleteAllBtn = document.querySelector(".del-button");
 
 
 addBtn.onclick = ()=>{
+  codeNum=0;
   document.querySelector(".text-preview").style.display="block";
   document.querySelector(".text-blur").style.display="block";
 }
@@ -57,16 +60,18 @@ function getItems(){
   })
 }
 
-function addItem(text){
+function addItem(text,num){
   console.log("Added item");
   let newItem = db.collection("notes").add({
-      text: text
+      text: text,
+      code: num
   })
 }
 
-function setItem(index, text){
+function setItem(index, text, num){
   let newItem = db.collection("notes").doc(index).set({
-    text: text
+    text: text,
+    code: num
   })
 }
 
@@ -76,14 +81,16 @@ saveBtn.onclick = ()=>{
   if(userEnteredValue.trim() != 0){
   //console.log("savebtn "+userEnteredValue); 
   if(idx==null){
-  addItem(userEnteredValue);}
+  addItem(userEnteredValue, codeNum);}
   else{
-    setItem(idx, userEnteredValue);
+    setItem(idx, userEnteredValue, codeNum);
     idx=null;
   }
   getItems();
   //showTasks(); 
   saveBtn.classList.remove("active"); 
+  codeNum=0;
+  impBtn.classList.remove("active");
   document.querySelector(".text-preview").style.display="none";
   document.querySelector(".text-blur").style.display="none";
   }
@@ -91,9 +98,20 @@ saveBtn.onclick = ()=>{
 
 function showTasks(items){
   let todoItems = []
+  console.log("code "+codeNum);
     items.forEach((item) => {
         let todoItem = document.createElement("div");
         todoItem.classList.add("notes-list-item");
+        /*if(item.code==1){
+            todoItem.style.background="#6fb4798a";
+        }*/
+        let topBar=document.createElement("div");
+        topBar.classList.add("topBar");
+        let impMark=document.createElement("div");
+        impMark.classList.add("mark");
+        if(item.code==1){
+            impMark.style.background="#2ebce0ab";
+        }
         let checkContainer = document.createElement("div");
         checkContainer.classList.add("icon");
         let checkMark = document.createElement("div");
@@ -101,19 +119,21 @@ function showTasks(items){
         checkMark.addEventListener("click", function(){
             editTask(item.id);
         })
-        let checkMark1 = document.createElement("i");
-        checkMark1.innerHTML = '<i class="far fa-times-circle fa-lg" id="exit"></i>';
-        checkMark1.addEventListener("click", function(){
+        let delMark = document.createElement("div");
+        delMark.innerHTML = '<i class="far fa-times-circle fa-lg" id="exit"></i>';
+        delMark.addEventListener("click", function(){
             deleteTask(item.id);
         })
         checkContainer.appendChild(checkMark);
-        checkContainer.appendChild(checkMark1);
+        checkContainer.appendChild(delMark);
+        topBar.appendChild(impMark);
+        topBar.appendChild(checkContainer);
 
         let todoText = document.createElement("div");
         todoText.classList.add("small-text-body");
         todoText.innerText = item.text;
 
-        todoItem.appendChild(checkContainer);
+        todoItem.appendChild(topBar);
         todoItem.appendChild(todoText);
         todoItems.push(todoItem)
     })
@@ -131,9 +151,15 @@ function deleteTask(index){
 function editTask(index){
   let docRef=db.collection("notes").doc(index);
   inputBox.value="Please Wait...";
+  codeNum=0;
   docRef.get().then((doc) => {
         console.log("Document data:", doc.data());
         inputBox.value=doc.data().text;
+        codeNum=doc.data().code;
+        if(codeNum==1){
+          impBtn.classList.add("active");
+        }
+        console.log("code "+codeNum);
   })
   idx=index;
   document.querySelector(".text-preview").style.display="block";
@@ -142,12 +168,24 @@ function editTask(index){
   console.log("updated item");
 }
 
+impBtn.onclick = () =>{
+  if(impBtn.className=="imp active"){
+    impBtn.classList.remove("active");
+    codeNum=0;
+  }
+  else{
+    impBtn.classList.add("active");
+    codeNum=1;
+  }
+  console.log("code "+codeNum);
+}
+
 const btn_toggle = document.querySelector(".theme-toggle");
 const theme = document.querySelector("#theme-link");
-      btn_toggle.addEventListener("click", function() {
-        if (theme.getAttribute("href") == "./css/light-style.css") {
-          theme.href = "./css/dark-style.css";
-        } else {
-          theme.href = "./css/light-style.css";
-        }
-      });
+btn_toggle.addEventListener("click", function() {
+  if (theme.getAttribute("href") == "./css/light-style.css") {
+    theme.href = "./css/dark-style.css";
+  } else {
+    theme.href = "./css/light-style.css";
+  }
+});
